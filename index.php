d881a74840f2653ec0e292341c4c7c26486a5b09
Alexander Wallin <office@alexanderwallin.com>
2016-03-02 19:41:16 +0100

    Cache bust

--- index.php
+++ index.php
@@ -266,7 +266,7 @@ $isProduction = preg_match("/\.com/", $_SERVER['HTTP_HOST']);
 		<?php if ($isProduction) : ?>
 
 		<!-- Data scripts -->
-		<script defer src="js/audanism.js?v=20140717-1"></script>
+		<script defer src="js/audanism.js?v=2016-03-02"></script>
 
 		<?php else : ?>
 
